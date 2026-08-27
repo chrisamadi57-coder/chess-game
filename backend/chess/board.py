f3b490b2dba28b6c5ba1c9cdf6612f0cc4c889a5
dgbaga <destinygbaga51@gmail.com>
2026-08-27 10:16:31 +0100

    Backend

---
+++ backend/chess/board.py
@@ -0,0 +1,12 @@
+class Board:
+    def __init__(self):
+        self.board = [
+            ["BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"]
+            ["BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP"]
+            [None, None, None, None, None, None, None, None]
+            [None, None, None, None, None, None, None, None]
+            [None, None, None, None, None, None, None, None]
+            [None, None, None, None, None, None, None, None]
+            ["WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP"]
+            ["WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"]
+        ]
